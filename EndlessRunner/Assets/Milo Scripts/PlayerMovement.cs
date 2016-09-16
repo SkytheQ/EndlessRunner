51df5efc2a0248b711b38c22ccb2c2e2f5110c5c
MrElektronix <milovanpelt@gmail.com>
2016-09-16 18:18:56 +0200

    PlayerMovement

---
+++ EndlessRunner/Assets/Milo Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private float RunSpeed = 10f;
+	private float JumpSpeed = 10f;
+	public Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float run = Input.GetAxis ("Horizontal") * RunSpeed;
+		run *= Time.deltaTime;
+		transform.Translate (run, 0, 0);
+
+		float jump = Input.GetAxis ("Vertical") * JumpSpeed;
+		jump *= Time.deltaTime;
+		transform.Translate (0, jump, 0);
+		rb.AddForce (Vector3.up * jump, ForceMode.Impulse);
+
+	}
+}
