d29e6330e0c594c0e494610fb63caabc4ecf84f1
MrElektronix <milovanpelt@gmail.com>
2016-09-16 17:55:17 +0200

    player beginning

---
+++ Arduino Controller/Assets/controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class controller : MonoBehaviour {
+	SerialPort sp = new SerialPort("COM3", 9600);
+
+	void Start () {
+		sp.Open ();
+		sp.ReadTimeout = 1;
+	}
+
+	void Update () 
+	{
+		try{
+			print (sp.ReadLine());
+		}
+		catch(System.Exception){
+		}
+	}
+}
---
+++ EndlessRunner/Assets/Milo Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	private float RunSpeed = 10f;
+	private float JumpSpeed = 30f;
+	private float maxJumpHeight = 1f;
+	private bool jumped = false;
+
+	private Rigidbody rb;
+
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float run = Input.GetAxis ("Horizontal") * RunSpeed;
+		run *= Time.deltaTime;
+		transform.Translate(run, 0, 0);
+
+		if (transform.position.y >= maxJumpHeight) {
+			return;
+		} else {
+			float jump = Input.GetAxisRaw ("Vertical") * JumpSpeed;
+			rb.AddForce (Vector3.up * jump * Time.deltaTime, ForceMode.Impulse);
+		}
+
+	}
+}
