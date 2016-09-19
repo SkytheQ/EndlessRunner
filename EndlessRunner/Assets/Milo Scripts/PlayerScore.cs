9ec8de66cef5f5874dc89f03dea814c6556f3ad0
MrElektronix <milovanpelt@gmail.com>
2016-09-19 23:22:54 +0200

    EndlessRunner 19 september 23:20
    - Added Score
    - Added Enemy to test score decreasement when player hits Enemy

---
+++ EndlessRunner/Assets/Milo Scripts/PlayerScore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class PlayerScore : MonoBehaviour {
+
+	public Text scoreText;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 100;
+		setScoreText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Enemy")) {
+			if (score == 0) {
+				score = 0;
+			} else {
+				score -= 20;
+			}
+		
+			setScoreText ();
+		}
+	}
+
+	void setScoreText() {
+		scoreText.text = "Score: " + score.ToString();
+	}
+}
