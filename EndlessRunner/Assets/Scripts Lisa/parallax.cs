7070c8ef7770a28d526c46399923aa95960df039
SkytheQ <lieskuh11@hotmail.com>
2016-09-23 11:01:11 +0200

    background stuff

---
+++ EndlessRunner/Assets/Scripts Lisa/parallax.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parallax : MonoBehaviour {
+
+    public float parralax;
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteRenderer mr = GetComponent<SpriteRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        mat.mainTextureOffset = offset;
+    }
+}
