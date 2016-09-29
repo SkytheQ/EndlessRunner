44e80f7b1a9d94ee2e0cdd46512ded55c295bf00
MrElektronix <milovanpelt@gmail.com>
2016-09-29 10:29:18 +0200

    Revert "EndlessRunner 29 september 09:41"
    This reverts commit b3e22c0339cfbc7fd1eb755b89eb924c8eaf3f60.

--- EndlessRunner/Assets/Milo Scripts/PlayerAttack.cs
+++ EndlessRunner/Assets/Milo Scripts/PlayerAttack.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class PlayerAttack : MonoBehaviour {
 
-	[SerializeField] private Transform EnemyCheck;
-
 	// Use this for initialization
 	void Start () {
 		
@@ -13,17 +11,5 @@ public class PlayerAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void FixedUpdate() {
-		RaycastHit hit;
-		if (Physics.Linecast (transform.position, /*-Vector2.up*/ EnemyCheck.position, out hit)) {
-			print("touch hit: " + hit.collider.gameObject.name);
-			if (Input.GetKeyDown (KeyCode.E)) {
-				Destroy (hit.collider.gameObject);
-			}
-		} else {
-			print("Did not touch Enemy");
-		}
 	}
 }
--- EndlessRunner/Assets/Milo Scripts/PlayerMovement.cs
+++ EndlessRunner/Assets/Milo Scripts/PlayerMovement.cs
@@ -8,7 +8,7 @@ public class PlayerMovement : MonoBehaviour {
 	private float JumpSpeed = 8f;
 	private bool isFalling = false;
 	public Rigidbody rb;
-	[SerializeField] private Transform groundCheck;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
@@ -32,26 +32,14 @@ public class PlayerMovement : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		/*
 		if (Input.GetKeyDown (KeyCode.W) && isFalling == false) {
 			rb.AddForce (Vector3.up * JumpSpeed, ForceMode.VelocityChange);
 		}
 		isFalling = true;
-		*/
 		//Debug.Log (JumpSpeed);
-		//Debug.DrawLine(transform.position, groundCheck.position);
-		//Debug.Log ("Player position: " + transform.position);
-		//Debug.Log ("ground position: " + groundCheck.position);
-		if (Physics.Linecast (transform.position, /*-Vector2.up*/ groundCheck.position)) {
-			if (Input.GetKeyDown (KeyCode.W)) {
-				rb.AddForce (Vector3.up * JumpSpeed, ForceMode.VelocityChange);
-			}
-		} else {
-		}
 	}
-	/*
+
 	void OnCollisionStay() {
 		isFalling = false;
 	}
-	*/
 }
