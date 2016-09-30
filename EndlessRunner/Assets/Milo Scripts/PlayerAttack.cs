540067080c8e733eef6cc642753267f3951981a9
MrElektronix <milovanpelt@gmail.com>
2016-09-30 12:01:23 +0200

    EndlessRunner 30 september 12:01
    - Added life's
    - You now get 50 points when you kill a enemy

--- EndlessRunner/Assets/Milo Scripts/PlayerAttack.cs
+++ EndlessRunner/Assets/Milo Scripts/PlayerAttack.cs
@@ -17,11 +17,16 @@ public class PlayerAttack : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
+		GameObject Player = GameObject.Find ("Player");
+		PlayerScore_UI playerscore_ui = Player.GetComponent<PlayerScore_UI> ();
+
 		RaycastHit hit;
 		if (Physics.Linecast (transform.position, enemyCheck.position, out hit)) {
 			Debug.Log ("enemy hit");
 			if (Input.GetKeyDown (KeyCode.E)) {
 				Destroy (hit.collider.gameObject);
+				playerscore_ui.score += 50;
+				playerscore_ui.SetScoreText ();
 			}
 		} else {
 			Debug.Log ("did not hit enemy");
---
+++ EndlessRunner/Assets/Milo Scripts/PlayerLife.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerLife : MonoBehaviour {
+
+	[SerializeField]
+	private Text lifeText;
+	private int Lifes;
+
+	// Use this for initialization
+	void Start () {
+		Lifes = 3;
+		lifeText.text = "Life's: " + Lifes.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
