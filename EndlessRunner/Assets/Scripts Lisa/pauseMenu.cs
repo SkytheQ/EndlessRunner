697436a301e7a63cf3b4c0ef5f8703ba69a4a9d9
SkytheQ <lieskuh11@hotmail.com>
2016-10-14 21:53:45 +0200

    pause menu + main

---
+++ EndlessRunner/Assets/Scripts Lisa/pauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject pauseUI;
+
+    private bool _paused = false;
+
+    void Start()
+    {
+        pauseUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            _paused = !_paused;
+        }
+
+        if (_paused)
+        {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0;
+            AudioListener.pause = true;
+            
+        }
+
+        if (!_paused)
+        {
+            pauseUI.SetActive(false);
+            Time.timeScale = 1;
+            AudioListener.pause = false;
+        }
+    }
+
+    public void Resume()
+    {
+        _paused = false;
+    }
+
+    public void Restart(string _restart)
+    {
+        SceneManager.LoadScene(_restart);
+
+    }
+
+    public void MainMenu(string _mainMenu)
+    {
+        SceneManager.LoadScene(_mainMenu);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
