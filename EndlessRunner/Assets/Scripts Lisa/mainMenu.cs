98eb79579af064ddd12dfe19083095c2ad0718a2
SkytheQ <lieskuh11@hotmail.com>
2016-10-14 12:37:54 +0200

    menu shit

---
+++ EndlessRunner/Assets/Scripts Lisa/mainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour {
+
+    public void loadLevel(string _loadLevel)
+    {
+        SceneManager.LoadScene(_loadLevel);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+}
